d5810de18d769c87017e2d723bb840a97f4d70ca
aStevee <sfukinguay@gmail.com>
2023-09-06 20:34:03 +0200

    first files added

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You might want to specify exact origins in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def main():
+    return {"Response":"This is a test!"}
+
